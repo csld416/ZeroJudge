85c842be499fd2bc093e3c6ee453582455c9f81c
Chris <Chris@LAPTOP-VRVCOUAM>
2022-02-03 13:51:28 +0800

    2022/2/2

--- zerojudge/src/a671.java
+++ zerojudge/src/a671.java
@@ -1,4 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class a671 {
+	
+	final static Scanner sc = new Scanner(System.in);
+	final static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	public static void solve(double n, double p) {
+		double e = Math.E;
+		System.out.printf("%d\n", (int)(Math.pow(e, Math.log(p)/n) + 0.5));
+	}
+	
+	public static void main(String[] args) {
+		while(sc.hasNext()) {
+			double a  = sc.nextDouble();
+			double b = sc.nextDouble();
+			solve(a, b);
+		}
+	}
 
 }
--- zerojudge/src/c089.java
+++ zerojudge/src/c089.java
@@ -1,4 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class c089 {
+	
+	final static Scanner sc = new Scanner(System.in);
+	final static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	
+	public static void solve(String s) {
+		String w[] = s.split("\\s+");
+		String input = w[0];
+		int convert =Integer.parseInt(w[1]);
+		int output = Integer.parseInt(w[2]);
+		
+		int tmp = Integer.parseInt(input, convert);
+		System.out.println();
+
+	}
+	
+	public static void main(String[] args) {
+		while(sc.hasNext()) {
+			String s = sc.nextLine();
+			solve(s);
+		}
+	}
 
 }
